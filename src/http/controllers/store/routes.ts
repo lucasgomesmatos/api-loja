d955de1415019814037d579418bbf7507a5ce9b0
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-03-24 23:40:16 -0300

    feat: webhooks

--- src/http/controllers/store/routes.ts
+++ src/http/controllers/store/routes.ts
@@ -6,9 +6,9 @@ import { refreshTokenStore } from "./refresh";
 import { registerUserStore } from "./register";
 
 export async function usersStoreRoutes(app: FastifyInstance) {
-  app.post("/store/users", registerUserStore);
-  app.post("/store/sessions", authenticateUserStore);
+  app.post("/store/webhook", registerUserStore);
 
+  app.post("/store/sessions", authenticateUserStore);
   app.post("/store/token/refresh", refreshTokenStore);
   app.get("/store/products", { onRequest: [verifyJwt] }, getAllProductsByUser);
 }
